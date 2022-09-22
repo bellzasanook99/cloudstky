fe5e2e27b81b11a31e1298c84b6a42d2ffef447c
sitthichai <nookhinaken@gmail.com>
2022-09-23 01:49:25 +0700

    add git ignore

--- cloudstky/cloudstky/Startup.cs
+++ cloudstky/cloudstky/Startup.cs
@@ -39,6 +39,7 @@ namespace cloudstky
 
 
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IProductsService, ProductsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
