3297d1fddd3529b8da808250cad00da695eebf3a
sitthichai <nookhinaken@gmail.com>
2022-09-23 00:58:45 +0700

    add Products

---
+++ cloudstky/cloudstky/Services/ProductsService.cs
@@ -0,0 +1,24 @@
+﻿using cloudstky.Models;
+using cloudstky.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace cloudstky.Services
+{
+    public class ProductsService : IProductsService
+    {
+        private CloudStokyDBContext CloudStokyDBContext { get; }
+
+        public ProductsService(CloudStokyDBContext cloudStokyDBContext)
+        {
+            CloudStokyDBContext = cloudStokyDBContext;
+        }
+
+
+        public async Task<List<TblProduct>> Products()
+        {
+            return await CloudStokyDBContext.TblProducts.ToListAsync();
+        }
+    }
+}
